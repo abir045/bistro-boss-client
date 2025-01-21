f96868d7edd24868068ceed1428d79b73d12d35e
unknown <abir045@gmail.com>
2025-01-21 17:40:38 +0600

    updated admin home

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -105,7 +105,9 @@ export const router = createBrowserRouter([
           </AdminRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/menu/${params.id}`),
+          fetch(
+            `https://bistro-boss-server-six-azure.vercel.app/menu/${params.id}`
+          ),
       },
 
       //admin only routes
